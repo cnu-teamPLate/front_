291ff65091ce25f23d7d26dfd34cc2cfe9883019
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-04-03 19:58:16 +0900

    Update Assignment.js

--- src/pages/Assignment/Assignment.js
+++ src/pages/Assignment/Assignment.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import './Assignment.css';
 import React, { useState, useEffect } from 'react';
 import { NotificationPopup } from '../../components/NotificationPopup/NotificationPopup';
