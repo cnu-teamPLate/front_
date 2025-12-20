c9d5098f5fd0a43affd293f239d2e0ee04df827b
Amyleefiwfjj <2804amylee@naver.com>
2025-12-20 15:09:40 +0900

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 
 const Footer = () => {
-    return (
-        <footer>
-        <p>© 2024 CNU</p>
-        <div className="footer-links">
-          <a href="/about">About Us</a> | <a href="/contact">Contact</a> |{' '}
-          <a href="/privacy">Privacy Policy</a>
-        </div>
-      </footer>
-    );
+  return (
+    <footer>
+      <p>© 2026 CNU</p>
+      <div className="footer-links">
+        <a href="/about">About Us</a> | <a href="/contact">Contact</a> |{' '}
+        <a href="/privacy">Privacy Policy</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
